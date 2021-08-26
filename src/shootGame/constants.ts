687d3548a9bb71f16b7d887f7e7b441920f21e88
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-08-26 13:43:18 +0200

    Arena drawing

--- src/shootGame/constants.ts
+++ src/shootGame/constants.ts
@@ -1,2 +1,8 @@
+import { Color } from "../drawer/Color"
+
 export const BULLET_SPEED = 30
 export const PLAYER_BULLET_TIMEOUT = 0.1
+export const ARENA_RADIUS = 30
+export const ARENA_RADIUS_SQR = ARENA_RADIUS ** 2
+export const TILE_SIZE = 1
+export const TILE_BASE_COLOR = new Color(0.043, 0.086, 0.114)
--- src/shootGame/rendering/RenderDirector.ts
+++ src/shootGame/rendering/RenderDirector.ts
@@ -1,5 +1,7 @@
 import { Drawer } from "../../drawer/Drawer"
 import { Point } from "../../drawer/Point"
+import { Rect } from "../../drawer/Rect"
+import { ARENA_RADIUS, ARENA_RADIUS_SQR, TILE_BASE_COLOR, TILE_SIZE } from "../constants"
 import { Transform } from "../Transform"
 import { Camera } from "./Camera"
 import { DrawableComponent } from "./DrawableComponent"
@@ -24,6 +26,41 @@ export class RenderDirector {
     ) {
         try {
             RenderDirector.current = this
+
+            drawer.save()
+
+            drawer
+                .beginPath()
+                .arc(this.worldToScreen(Point.zero), (ARENA_RADIUS - 1) * this.zoom)
+                .clip()
+
+            const screenTileSize = TILE_SIZE * this.zoom
+
+            const oX = -((this.pos.x + 10000) % TILE_SIZE) * this.zoom
+            const oY = -((this.pos.y + 10000) % TILE_SIZE) * this.zoom
+
+            const bX = Math.floor(this.pos.x / TILE_SIZE) - this.center.x / this.zoom
+            const bY = Math.floor(this.pos.y / TILE_SIZE) - this.center.y / this.zoom
+
+            const sX = Math.floor(this.drawer.size.width / this.zoom / TILE_SIZE) + 1
+            const sY = Math.floor(this.drawer.size.height / this.zoom / TILE_SIZE) + 2
+            for (let x = -1; x < sX; x++)
+                for (let y = -1; y < sY; y++) {
+                    const screenX = x * screenTileSize + oX
+                    const screenY = y * screenTileSize + oY
+                    const realX = x + bX
+                    const realY = y + bY
+
+                    if (realX * realX + realY * realY > ARENA_RADIUS_SQR) continue
+
+                    const value = parseFloat("0." + Math.sin(realX + realY * 50).toString().substr(6))
+
+
+                    drawer
+                        .setFillStyle(TILE_BASE_COLOR.mul(0.5 + value * 0.5))
+                        .fillRect(new Rect(screenX, screenY, screenTileSize, screenTileSize))
+                }
+            drawer.restore()
 
             for (const sprite of this.camera.system.iterateComponents(DrawableComponent)) {
                 sprite.drawSprite(drawer, deltaTime)
