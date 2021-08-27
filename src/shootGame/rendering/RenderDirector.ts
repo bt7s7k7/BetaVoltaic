1b38861ec05e1b9fd4d25b515463922f202093d7
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-08-27 14:22:18 +0200

    Removed commented removed code

--- src/shootGame/rendering/RenderDirector.ts
+++ src/shootGame/rendering/RenderDirector.ts
@@ -22,41 +22,6 @@ export class RenderDirector {
     }
 
     protected drawFloor() {
-        /* this.drawer.save()
-
-        this.drawer
-            .beginPath()
-            .arc(this.worldToScreen(Point.zero), (ARENA_RADIUS) * this.zoom)
-            .clip()
-
-        const screenTileSize = TILE_SIZE * this.zoom
-
-        const oX = -((this.pos.x + 10000) % TILE_SIZE) * this.zoom
-        const oY = -((this.pos.y + 10000) % TILE_SIZE) * this.zoom
-
-        const bX = Math.floor(this.pos.x / TILE_SIZE) - this.center.x / this.zoom
-        const bY = Math.floor(this.pos.y / TILE_SIZE) - this.center.y / this.zoom
-
-        const sX = Math.floor(this.drawer.size.width / this.zoom / TILE_SIZE) + 1
-        const sY = Math.floor(this.drawer.size.height / this.zoom / TILE_SIZE) + 2
-        for (let x = -1; x < sX; x++)
-            for (let y = -1; y < sY; y++) {
-                const screenX = x * screenTileSize + oX
-                const screenY = y * screenTileSize + oY
-                const realX = x + bX
-                const realY = y + bY
-
-                if ((realX + TILE_SIZE / 2) * (realX + TILE_SIZE / 2) + (realY + TILE_SIZE / 2) * (realY + TILE_SIZE / 2) > ARENA_RADIUS_SQR) continue
-
-                const value = parseFloat("0." + Math.sin(realX + realY * 50).toString().substr(6))
-
-
-                this.drawer
-                    .setFillStyle(TILE_BASE_COLOR.mul(0.5 + value * 0.5))
-                    .fillRect(new Rect(screenX, screenY, screenTileSize, screenTileSize))
-            }
-        this.drawer.restore() */
-
         FloorRenderer.drawFloor(this.camera, this.drawer)
     }
 
