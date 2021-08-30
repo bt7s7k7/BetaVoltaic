43ae17b323cfa30f130076da1267686bc8d6e180
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-08-30 17:15:35 +0200

    Layout improvements for SettingsView

--- src/shootGame/SettingsView.tsx
+++ src/shootGame/SettingsView.tsx
@@ -13,35 +13,37 @@ export const SettingsView = eventDecorator(defineComponent({
     setup(props, ctx) {
         return () => (
             <div class="flex-fill flex column bg-black monospace user-select-none p-4">
-                <div class="flex-fill flex column gap-2 as-settings-view">
-                    <div class="label">General</div>
-                    <div class="category">
-                        <div onClick={() => Settings.value.forceLandscape = !Settings.value.forceLandscape}>Force landscape</div>
-                        <Toggle vModel={Settings.value.forceLandscape} />
-                        <div>Touch controls</div>
-                        <Toggle vModel={Settings.value.touchControls} />
-                        {Settings.value.touchControls && <>
-                            <div>Center fire joystick</div>
-                            <Toggle vModel={Settings.value.fireCenter} />
-                        </>}
-                    </div>
-                    <div class="label">Graphics</div>
-                    <div class="category">
-                        <div>Chromatic aberration</div>
-                        <Toggle vModel={Settings.value.aberration} />
-                        <div>Bloom</div>
-                        <Toggle vModel={Settings.value.bloom} />
-                        <div>Small screen mode</div>
-                        <Toggle vModel={Settings.value.smallScreen} />
-                    </div>
-                    <div class="label">Other</div>
-                    <div class="category">
-                        <div>Show FPS</div>
-                        <Toggle vModel={Settings.value.fpsMeter} />
-                        <div>Slow motion</div>
-                        <Toggle vModel={Settings.value.slowMotion} />
-                        <div>Invulnerability</div>
-                        <Toggle vModel={Settings.value.invulnerability} />
+                <div class="flex-fill contain">
+                    <div class="absolute-fill scroll flex column gap-2 as-settings-view">
+                        <div class="label">General</div>
+                        <div class="category">
+                            <div onClick={() => Settings.value.forceLandscape = !Settings.value.forceLandscape}>Force landscape</div>
+                            <Toggle vModel={Settings.value.forceLandscape} />
+                            <div>Touch controls</div>
+                            <Toggle vModel={Settings.value.touchControls} />
+                            {Settings.value.touchControls && <>
+                                <div>Center fire joystick</div>
+                                <Toggle vModel={Settings.value.fireCenter} />
+                            </>}
+                        </div>
+                        <div class="label">Graphics</div>
+                        <div class="category">
+                            <div>Chromatic aberration</div>
+                            <Toggle vModel={Settings.value.aberration} />
+                            <div>Bloom</div>
+                            <Toggle vModel={Settings.value.bloom} />
+                            <div>Small screen mode</div>
+                            <Toggle vModel={Settings.value.smallScreen} />
+                        </div>
+                        <div class="label">Other</div>
+                        <div class="category">
+                            <div>Show FPS</div>
+                            <Toggle vModel={Settings.value.fpsMeter} />
+                            <div>Slow motion</div>
+                            <Toggle vModel={Settings.value.slowMotion} />
+                            <div>Invulnerability</div>
+                            <Toggle vModel={Settings.value.invulnerability} />
+                        </div>
                     </div>
                 </div>
                 <div class="flex row">
